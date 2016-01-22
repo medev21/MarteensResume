a50be705dca4dce5e87fff30b98774336a086da1
Martin Ben <mebenav88@gmail.com>
2016-01-22 16:18:05 -0500

    add Marteen class, and setup gem

--- README.md
+++ README.md
@@ -1,36 +1,28 @@
-# MarteensResume
-
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/Marteens_resume`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+# Get Started w/ MarteensResume
 
 ## Installation
 
-Add this line to your application's Gemfile:
-
-```ruby
-gem 'Marteens_resume'
-```
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
-
     $ gem install Marteens_resume
 
-## Usage
+## Load REPL in the terminal
 
-TODO: Write usage instructions here
+  irb or pry
 
-## Development
+## Using the gem
 
-After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake false` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+### Type the following in the irb/pry session:
 
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
+require 'Marteens_resume'
+include 'MarteensResume'
 
-## Contributing
+## Get Resume info
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/Marteens_resume. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](contributor-covenant.org) code of conduct.
-
+### Type any of the following for info
+Marteen.get_resume
+Marteen.contact
+Marteen.education
+Marteen.projects
+Marteen.experience
+Marteen.skills
+Marteen.social_media
+Marteen.message
--- lib/Marteens_resume.rb
+++ lib/Marteens_resume.rb
@@ -1,5 +1,112 @@
 require "Marteens_resume/version"
 
 module MarteensResume
-  # Your code goes here...
+  class Marteen
+    def self.contact
+      @contact = {
+        name: "Martin (Marteen) Benavides",
+        location: "Brooklyn, New York",
+        phone_number: "615-260-5740",
+        email: "mebenav88@gmail.com"
+      }
+
+      puts get_output(@contact)
+    end
+
+    def self.education
+      @edu = {
+        Startup_Institute: "Web Development Track, Fall 2015 Cohort",
+        Nashville_Community_College: "Computer Science, Spring 2015 -> non-degree, took a few courses just for fun!",
+        Tennessee_Tech_University: "B.S Mechanical Engineer, May 2012"
+      }
+
+      puts get_output(@edu)
+    end
+
+    def self.projects
+      @projects = {
+        college_perks: "a rails landing page with subscription functionality using MailChimp API -> campusperk.com",
+        kanban_card: "a trello clone app using rails, bootstrap, and jquery"
+      }
+
+      puts get_output(@projects)
+    end
+
+    def self.experience
+      @experience = {
+        Jpal: "Integrated Localize API to translate company’s site into different languages -> intern",
+        Bstow: "Helped company’s founders to promote customer acquisition -> intern",
+        Startup_Institute: "Selected from a competitive applicant pool for this eight week immersive program. "\
+                "Startup Institute helps individuals to develop the skills, mindset, and network to "\
+                "be high-impact at a high-growth company.",
+        Smelter_Service: "Engineer at an aluminum recycling industry where I improved daily work operations using "\
+                          "lean manufacturing methodologies",
+        Kysor_Warren: "Engineer intern where I designed supermarket refrigerators using 3D model software, SolidWorks"
+      }
+
+      puts get_output(@experience)
+    end
+
+    def self.skills
+      @skills = {
+        i_have_fun_with: "Ruby, Rails, HTML, CSS, JavaScript, Git, PostgresSQL, Python, Go, NodeJS"
+      }
+
+      puts get_output(@skills)
+    end
+
+    def self.social_media
+      @social = {
+        github: "https://github.com/medev21",
+        linkedin: "https://www.linkedin.com/in/medev21"
+      }
+
+      puts get_output(@social)
+    end
+
+    def self.message
+        puts '─────────▄──────────────▄ '
+        puts '────────▌▒█───────────▄▀▒▌ '
+        puts '────────▌▒▒▀▄───────▄▀▒▒▒▐ '
+        puts '───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐ '
+        puts '─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐ '
+        puts '───▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀██▀▒▌ '
+        puts '──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌ '
+        puts '──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐ '
+        puts '─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌ '
+        puts '─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌ '
+        puts '─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐ '
+        puts '▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒▒▒░░░░░░▒▒▒▒▌ '
+        puts '▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐ '
+        puts '─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌ '
+        puts '─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐ '
+        puts '──▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌ '
+        puts '────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀ '
+        puts '───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀ '
+        puts '──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀ '
+        puts ''
+        puts 'Thank you for checking my profile, looking forward to hear from you'
+        puts 'Available for hire, licensed to code'
+    end
+
+    def self.get_output(param)
+      @output = "\n"
+      param.each do |key, value|
+        @output += "#{key} : #{value}\n\n"
+      end
+
+      return @output
+    end
+
+    def self.get_resume
+      self.contact
+      self.education
+      self.projects
+      self.experience
+      self.skills
+      self.social_media
+      self.message
+    end
+
+  end
 end
